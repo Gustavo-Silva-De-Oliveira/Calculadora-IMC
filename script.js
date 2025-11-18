1b5fc345ed4b426b601501f8d422333abbb84312
Gustavo-Silva-De-Oliveira <gustavosdo04@gmail.com>
2025-11-17 21:30:43 -0300

    Criando nova funcionalidade de Calculadora de Taxa Metabolica e modificações no Script e Css!

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ function imc() {
   let peso = document.getElementById("peso").valueAsNumber;
   let altura = document.getElementById("altura").valueAsNumber;
 
+  // Exige que preencha os campos!
   if (!nome || isNaN(peso) || isNaN(altura || altura <= 0)) {
     alert("Preencha todos os campos!");
     return;
   }
+
   // Cria uma variável Valor com o Resultado do IMC.
   let valor = peso / (altura * altura);
   // Cria uma variável Mensagem.
@@ -33,5 +35,51 @@ function imc() {
   // Faz a animação
   requestAnimationFrame(() => {
     novoResultado.classList.add("anima");
+  });
+}
+
+// CALCULADORA METABOLICA
+function metabolica() {
+  let nome = document.getElementById("nome").value;
+  let peso = document.getElementById("peso").valueAsNumber;
+  let altura = document.getElementById("altura").valueAsNumber;
+  let idade = document.getElementById("idade").valueAsNumber;
+  let sexo = document.querySelector('input[name="sexo"]:checked').value;
+  let tmb;
+
+  // Exige que preencha os campos!
+  if (!nome || isNaN(peso) || isNaN(altura || altura <= 0)) {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  // Calcula para o Sexo Masculino.
+  if (sexo === "homem") {
+    peso = peso * 10;
+    altura = 6.25 * altura;
+    idade = 5 * idade;
+
+    tmb = peso + altura - idade + 5;
+
+    // Calcula para o Sexo Feminino.
+  } else if (sexo === "mulher") {
+    peso = peso * 10;
+    altura = 6.25 * altura;
+    idade = 5 * idade;
+
+    tmb = peso + altura - idade - 161;
+  }
+
+  // Cria histórico de taxas!
+  const container = document.getElementById("resultado");
+  const resultado = document.createElement("p");
+  resultado.textContent = `${nome}, sua Taxa Metabolica é ${tmb.toFixed(
+    2
+  )} Kcal!.`;
+  container.appendChild(resultado);
+
+  // Faz a animação
+  requestAnimationFrame(() => {
+    resultado.classList.add("anima");
   });
 }
